73e61ae2758b96a7dc23f2ac2932f63cb7f1db05
navnit0707 <knavnit81@gmail.com>
2023-08-08 17:40:11 +0530

    taking value from command line and printing it

--- main.go
+++ main.go
@@ -1,8 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main(){
 
-	fmt.Println("hello world")
+	var s,sep string
+	for i:= 0;i< len(os.Args);i++{
+		s += sep+ os.Args[i]
+		sep = " "
+
+	}
+	fmt.Println(s)
+	// result will be :   C:\Users\navni\AppData\Local\Temp\go-build319144518\b001\exe\main.exe hello hi
+	// Arg[0] is the name of command itself
 }
